3c94eba4129b7500b732954eee53e672a73dbb8e
Péter Vándor <peter.vandor@stud.uni-corvinus.hu>
2021-10-01 10:46:49 +0200

    Create default UI

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        //BindingList<User>
+
         public Form1()
         {
             InitializeComponent();
+
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
         }
     }
 }
