363e29f2f20b9386e3a4a4ba725ce0ef860c1c9a
Péter Vándor <peter.vandor@stud.uni-corvinus.hu>
2021-10-01 11:03:32 +0200

    Add function and Refactor name handling

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -13,15 +13,28 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
-        //BindingList<User>
+        BindingList<User> users = new BindingList<User>();
 
         public Form1()
         {
             InitializeComponent();
 
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FullName = textBox1.Text,
+            };
+                users.Add(u);
+        }
+
     }
 }
