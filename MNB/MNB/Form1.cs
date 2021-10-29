9e3177003a77ef18fb0e73d14eaeab0244677882
Péter Vándor <peter.vandor@stud.uni-corvinus.hu>
2021-10-29 10:16:55 +0200

    Add Exchange Rate List

--- MNB/MNB/Form1.cs
+++ MNB/MNB/Form1.cs
@@ -1,9 +1,11 @@
-﻿using MNB.MnbServiceReference;
+﻿using MNB.Entities;
+using MNB.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +15,13 @@ namespace MNB
 {
     public partial class Form1 : Form
     {
+
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             Consume();
+            dataGridView1.DataSource = Rates;
         }
 
         void Consume() 
@@ -25,10 +30,11 @@ namespace MNB
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
             request.currencyNames = "EUR";
             request.startDate = "2020-01-01";
-                request.endDate = "2020-06-30";
+            request.endDate = "2020-06-30";
             mnbService.GetExchangeRates(request);
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
         }
     }
 }
