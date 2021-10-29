57fac438e77865fc58e4e5a3ad6924b74db53efc
Péter Vándor <peter.vandor@stud.uni-corvinus.hu>
2021-10-29 10:29:08 +0200

    Add LoadXML

--- MNB/MNB/Form1.cs
+++ MNB/MNB/Form1.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace MNB
 {
@@ -20,11 +21,12 @@ namespace MNB
         public Form1()
         {
             InitializeComponent();
-            Consume();
+            string xmlstring = Consume();
+            LoadXML(xmlstring);
             dataGridView1.DataSource = Rates;
         }
 
-        void Consume() 
+        string Consume() 
         {
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
@@ -34,7 +36,27 @@ namespace MNB
             mnbService.GetExchangeRates(request);
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
-            File.WriteAllText("export.xml", result);
+            return result;
+        }
+
+        private void LoadXML(string input) 
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(input);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                XmlElement child = (XmlElement)item.FirstChild; //ChildNodes[0]
+                r.Currency = child.GetAttribute("curr");
+                r.Value = decimal.Parse(child.InnerText);
+                int unit = int.Parse(child.GetAttribute("unit"));
+                if (unit != 0)
+                {
+                    r.Value = r.Value / unit;
+                }
+                Rates.Add(r);
+            }
         }
     }
 }
