55d88d2bb1fa886a3c98ee7abdb2e41b82910e13
corvinus-jgfgc7 <vandorpeter2000@gmail.com>
2021-11-12 10:30:32 +0100

    Add BallFactory class, create conveyor belt

--- SantaFactory/SantaFactory/Form1.cs
+++ SantaFactory/SantaFactory/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SantaFactory.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,52 @@ namespace SantaFactory
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _ballfactory;
+
+        public BallFactory BallFactory
+        {
+            get { return _ballfactory; }
+            set { _ballfactory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            BallFactory = new BallFactory();
+
+            var b = BallFactory.CreateNew();
         }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = BallFactory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            ball.Left = -ball.Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var lastPosition = 0;
+            foreach (var item in _balls)
+            {
+                item.MoveBall();
+                if (item.Left > lastPosition)
+                {
+                    lastPosition = item.Left;
+                }
+            }
+
+            if (lastPosition >= 1000)
+            {
+                var oldestBall = _balls[0];
+                _balls.Remove(oldestBall);
+                mainPanel.Controls.Remove(oldestBall);
+            }
+        }
+
     }
 }
